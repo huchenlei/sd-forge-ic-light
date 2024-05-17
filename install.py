700dbc5e2586c0ca646f99cfed47674c2365a5f9
Haoming <hmstudy02@gmail.com>
2024-05-17 19:24:58 +0800

    fix deps

--- install.py
+++ install.py
@@ -3,8 +3,9 @@ import launch
 if not launch.is_installed("rembg"):
     launch.run_pip("install rembg==2.0.50 --no-deps", "rembg")
 
-if not launch.is_installed("onnxruntime"):
-    launch.run_pip("install onnxruntime", "rembg")
+for dep in ("onnxruntime", "pymatting", "pooch"):
+    if not launch.is_installed(dep):
+        launch.run_pip(f"install {dep}", f"{dep} for rembg")
 
 if not launch.is_installed("pydantic"):
-    launch.run_pip("install pydantic~=1.10.11", "rembg")
+    launch.run_pip("install pydantic~=1.10.11", "pydantic for ic-light")
